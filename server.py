a8335da53cf05fdcf2ce56dcd46396284a1135fb
Ananth Sankaralingam <79415395+AnanthSankaralingam@users.noreply.github.com>
2024-08-11 17:47:05 -0700

    trying app runner

--- server.py
+++ server.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 from router import setup_routes
-# import awsgi
-# pip install Flask octoai chromadb==0.4.14 
 
 '''
-Create and run server on aws lambda, since we dont expect much traffic.
+Create and run server on AWS Lambda, since we don't expect much traffic.
 logging flask_cors octoai
-chromadb==0.4.14  asyncio dotenv
+chromadb==0.4.14 asyncio dotenv
 '''
 
 def create_app():
@@ -20,8 +18,10 @@ def create_app():
 
 app = create_app()
 
+# Uncomment the following if you decide to deploy on AWS Lambda
 # def lambda_handler(event, context):
 #     return awsgi.response(app, event, context, base64_content_types={"image/png"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Ensure the app listens on all interfaces and port 8080, as required by AWS App Runner
+    app.run(host='0.0.0.0', port=8080, debug=True)
